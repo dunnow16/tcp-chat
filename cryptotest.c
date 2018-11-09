0d62d94cafe99682abebb9296486c7f58ddfc345
Owen Dunn <dunnow@mail.gvsu.edu>
2018-11-09 14:48:42 -0500

    added Owen's notes

--- cryptotest.c
+++ cryptotest.c
@@ -79,6 +79,7 @@ int rsa_encrypt(unsigned char* in, size_t inlen, EVP_PKEY *key, unsigned char* o
   return outlen;
 }
 
+// a wrapper function: use as is easiest way (may use inside functions yourself)
 int rsa_decrypt(unsigned char* in, size_t inlen, EVP_PKEY *key, unsigned char* out){ 
   EVP_PKEY_CTX *ctx;
   size_t outlen;
@@ -130,6 +131,9 @@ int decrypt(unsigned char *ciphertext, int ciphertext_len, unsigned char *key,
   return plaintext_len;
 }
 
+// takes a msg, encrypts and dec right away
+// non standard lib functions used
+// integrate with chat program:
 int main(void){
   // Public key
   unsigned char *pubfilename = "RSApub.pem";
@@ -149,7 +153,9 @@ int main(void){
   // Fill an array of 32 characters with 32 random bytes to use as
   // symmetric key.
   // Use this function because others may not be random enough.
-  RAND_bytes(key,32);
+  // generate random key using good random bytes 
+  // (use rand functions from a security lib best practice)
+  RAND_bytes(key,32); 
   // Create initialization vector.  Recreate for each message.
   // Why pseudo random?  Because it's only important to have iv different
   // for each message.  Pseudo rand is a little more efficient
@@ -157,7 +163,7 @@ int main(void){
   EVP_PKEY *pubkey, *privkey;
   // Read public key from file
   FILE* pubf = fopen(pubfilename,"rb");
-  pubkey = PEM_read_PUBKEY(pubf,NULL,NULL,NULL);
+  pubkey = PEM_read_PUBKEY(pubf,NULL,NULL,NULL);  // read public key
   unsigned char encrypted_key[256];
   // Encrypt symmetric key using public key.
   // key - thing we want to encrypt
@@ -173,6 +179,19 @@ int main(void){
   // ciphertext - output buffer, filled with encrypted message
   // Returns length of encrypted message -- not guaranteed to be same length
   // as original message!  Will always be a multiple of 16 bytes
+  // enc using a string of bytes (can be any char array), uses lib functions (see above)
+  // (what trying to encrypt, len of input (check to make sure will fit buffer length), 
+  // key we're using (symmetric key - RSA is assymmetric key), 
+  // initiazation vector, where output is going (can't assume encypt text is same 
+  // length as original msg (multiple of 16 bytes)))
+  // init vector: When enc multiple msgs using a deterministic process will produce 
+  // the same result for the same msg. An attacker may be able to learn the same 
+  // thing was sent twice, etc.
+  // - used to provide some randomness: XOR iv with msg to prevent same msg from 
+  // looking the same; must use diff iv to accomplish this; randomly gen a new iv 
+  // every msg; send the iv over the 
+  // connection in clear text in a header (the key itself provides the secrecy) 
+  // so it can be used with key and msg to dec. 
   ciphertext_len = encrypt (plaintext, strlen ((char *)plaintext), key, iv,
                             ciphertext);
   printf("Ciphertext is:\n");
@@ -181,16 +200,19 @@ int main(void){
   BIO_dump_fp (stdout, (const char *)ciphertext, ciphertext_len);
 
   // Read private key
-  FILE* privf = fopen(privfilename,"rb");
+  // open file for private key and read priv key (only s should do this)
+  FILE* privf = fopen(privfilename,"rb"); 
   privkey = PEM_read_PrivateKey(privf,NULL,NULL,NULL);
   unsigned char decrypted_key[32];
   // This is what the server would do.
+  // enc using priv key (, the length of the key, , output)
   int decryptedkey_len = rsa_decrypt(encrypted_key, encryptedkey_len, privkey, decrypted_key); 
   // Decrypt using symmetric key
   // Need to decrypt using same initialization vector.  How to give iv
   // to server?  Just tack it on to the front, unencrypted.  No harm, because
   // iv isn't meant to prevent decryption -- that's what the key is for.
   // iv is just to prevent patterns from being discovered.
+  // ( , , , , decrypted text buffer)
   decryptedtext_len = decrypt(ciphertext, ciphertext_len, decrypted_key, iv,
 			      decryptedtext);
   decryptedtext[decryptedtext_len] = '\0';
